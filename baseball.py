dd9919f0d0aec315ee7291ee60ebc9c31a18fe4e
EC2 Default User <ec2-user@ip-172-31-47-27.us-east-2.compute.internal>
2019-07-18 01:28:57 +0000

    initial commit

---
+++ baseball.py
@@ -0,0 +1,1 @@
+baseballTeamsEast = {"Braves": "Atlanta", "Marlins": "Miami", "Phillies": "Philadelphia", "Nationals": "Washington", "Orioles": "Baltimore", "Red Sox": "Boston", "Rays": "Tampa Bay", "Blue Jays": "Trenton", "Jets": "New York"}
---
+++ weekOneReview.py
@@ -0,0 +1,46 @@
+#1 Print your name to the console!
+
+
+#2 Ask for a user's birthday year and return how old they will turn this year.
+
+
+#3 Write a short program that asks the name of the user.  If they write in all caps, thank them for their enthusiasm.  Otherwise, ask them to speak louder next time.
+#NOT CRITICAL PATH: Make the program repeatedly ask the user for their name, until they finally comply and write it in all caps.
+
+
+
+#4 Write a function that tells whether a number is divisible by 3.
+
+
+
+#5 Write a function that takes in three numbers and tells the user which one is the largest.
+
+
+
+#6 Write a function that takes in the per-person cost of breakfast and the per-person cost of lunch, and then calculates the cost of all food for the three-week FinTech program.
+
+
+
+#7 Here is a list (which you will un-comment):
+#classesInHighSchool = ["Algebra", "French", "Physics", "Global History", "Gym", "English", "Biology", "Band"]
+# Add another course at the end of the list.  Print your new list.
+# Let's say you didn't take French in high school.  Replace French with a class you did take.  Print your new list.
+# Alphabetize the list.  Print your new list.
+# NOT CRITICAL PATH: Create a new list that contains only the 7-letter elements from your list.  Print this new list.
+
+
+
+#8a Write a line of code that will enable this Python file to pull from the baseballTeamsEast dictionary in baseball.py
+
+#8b Print the city where the Braves play.
+
+#8c Correct the city of the Blue Jays.  They actually play in Toronto.
+
+#8d Remove the entry for the Jets.  They play football, not baseball.
+
+#8e Add a key-value pair for the Yankees (or the Mets, if you have bad taste).
+
+
+
+#9 Uncomment the line below.  Then write code that will print the first element of the second element of the third element.
+#myAlphabet = [[["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"]], [["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r"]], [["s", "t", "u"], ["v", "w", "x"], ["y", "z", "end"]]]
